9eac0266b40ba17350bcf9088710b3b33ea9bbb9
Maksym <antimaks1974@gmail.com>
2025-02-01 16:21:53 +0100

    First commit

---
+++ booking/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Room(models.Model):
+    number = models.CharField(max_length=10)
+    capacity = models.IntegerField()
+    location = models.TextField(max_length=100) 
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.number
+
+class Booking(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='bookings')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bookings')
+    start_time = models.DateField()
+    end_time = models.DateField() 
+    creation_time = models.DateTimeField(auto_now_add=True) 
+
+    def __str__(self):
+        return self.room.number + " " + str(self.date) + " " + str(self.start_time) + " " + str(self.end_time) 
